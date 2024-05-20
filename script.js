b5819df2a5dabbf4ec52b428c053caab196c3872
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-05-20 11:27:05 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//Show current time 
+
+        /*let myVar = setInterval(myTimer, 1000); 
+        function myTimer() {
+            const d = new Date();
+            document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+        }*/
+
+//The window.navigator object contains information about the visitor's browser.
+
+        // document.getElementById("demo").innerHTML = "cookiesEnabled is " + navigator.cookieEnabled;
+
+//The window.history object contains the browsers history.
+
+        /*function goBack() {
+            window.history.back()    //clicking back in the browser
+        }*/
+
+//The window.location object can be used to get the current page address (URL) and to redirect the browser to a new page.
+
+        //document.getElementById("demo").innerHTML = "Page location is " + window.location.assign();
+
+//Print the current window
+
+        /*function printPage() {
+            window.print();
+        }*/
